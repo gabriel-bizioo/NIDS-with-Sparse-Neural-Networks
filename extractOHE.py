528ade440bae6cc6a081380bf900bfefd0be06e8
gabriel-bizioo <gabriel.bizio@ufpr.br>
2026-05-12 20:21:02 -0300

    Feito script para dataset preliminar

--- extractOHE.py
+++ extractOHE.py
@@ -1,27 +1,43 @@
 import argparse
+from ctypes import sizeof
 import sys
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing as skp
+from sklearn.datasets import  dump_svmlight_file
 
 parser = argparse.ArgumentParser(sys.argv[0])
 parser.add_argument("file", help="File to be loaded and processed", type=str)
 args = parser.parse_args()
 
-columns = ['Dst Port', 'Protocol', 'Label']
+columns = ['Dst Port', 'Protocol', 'Packet Length Std', 'Label']
 df = pd.read_csv(args.file, usecols=columns, low_memory=False)[columns]
 print(df.head())
 
+df.replace([np.inf, -np.inf], np.nan, inplace=True)
+df.dropna(inplace=True)
+
 le = skp.LabelEncoder()
-output = le.fit_transform(df['Label'])
+labels = le.fit_transform(df['Label'])
 print(list(le.classes_))
-print(output)
+count = list(le.classes_)
+print(count.__sizeof__())
+print(labels.size)
 
-df['Dst Port'] = df['Dst Port'].astype(str)
-df[Protocol] = df[Protocol].astype(str)
+ports = [str(i) for i in range(65536)]
+protocols = [str(i) for i in range(256)]
 
-features_ohe = df[['Dst Port', 'Protocol']]
+bins_pkt = [0, 64, 512, 1000, 1500, float('inf')]
+labels_pkt = ['Tiny', 'Small', 'Medium', 'Large', 'Jumbo']
+df['Pkt_Bin'] = pd.cut(df['Packet Length Std'], bins=bins_pkt, labels=labels_pkt)
+
+ohe = skp.OneHotEncoder(categories=[ports, protocols], sparse_output=True, handle_unknown='ignore')
+features_ohe = df[['Dst Port', 'Protocol', 'Pkt_Bin']]
 features_ohe = features_ohe.fillna(0).astype(int).astype(str)
-ohe = skp.OneHotEncoder(sparse_output=True, handle_unknown='ignore')
+
+
 output = ohe.fit_transform(features_ohe)
+print(output.shape)
+print(ohe.get_feature_names_out(['Dst Port', 'Protocol']))
 
-
+#dump_svmlight_file(output, labels, 'SparseCICIDSTest.txt', zero_based=True)
